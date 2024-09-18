59e5a7e8b4f23c8a101a59bd57b111408eb5eb3c
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-09-18 14:47:09 +0500

    right way to create todo app

--- src/pages/PracticeTodoapp.jsx
+++ src/pages/PracticeTodoapp.jsx
@@ -1,3 +1,75 @@
+// import React, { useState } from "react";
+
+// const PracticeTodoapp = () => {
+//   const [todos, setTodos] = useState([
+//     {
+//       value: "jawad",
+//       disabled: true,
+//     },
+//     {
+//       value: "zain",
+//       disabled: true,
+//     },
+//   ]);
+
+//   const [value, setValue] = useState("");
+
+//   const addTodos = () => {
+//     setTodos([...todos, { value, disabled: true }]);
+//     setValue("");
+//   };
+
+//   const editTodo = (v, i) => {
+//     todos.splice(i, 1, { ...v, disabled: false });
+//     setTodos([...todos]);
+//   };
+
+//   const updateTodo = (v, i) => {
+//     v.disabled = true;
+//     setTodos([...todos]);
+//   };
+
+//   const deleteTodo = (v, i) => {
+//     const oldTodos = [...todos];
+//     oldTodos.splice(i, 1);
+//     console.log(i, "<<< i");
+//     setTodos([...oldTodos]);
+//   };
+
+//   return (
+//     <div>
+//       <input type="text" onChange={(e) => setValue(e.target.value)} />
+//       <button onClick={() => addTodos()}>Add Todos</button>
+//       <button onClick={() => setTodos([])}>Delete all</button>
+//       <br />
+
+//       <ul>
+//         {todos.map((v, i) => <li key={i}>
+//             <input
+//             key={i}
+//               type="text"
+//               defaultValue={v.value}
+//               disabled={v.disabled}
+//               onChange={(e) => v.value = e.target.value}
+//             />
+//             {v.disabled ? (
+//               <button onClick={() => editTodo(v, i)}>Edit</button>
+//             ) : (
+//               <button onClick={() => updateTodo(v, i)}>Update</button>
+//             )}
+//             <button onClick={() => deleteTodo(i)}>Delete</button>
+//           </li>
+//         )}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default PracticeTodoapp;
+
+
+
+
 import React, { useState } from "react";
 
 const PracticeTodoapp = () => {
@@ -25,22 +97,24 @@ const PracticeTodoapp = () => {
   };
 
   const updateTodo = (v, i) => {
-    v.disabled = true;
+    todos[i].disabled = true; // Update specific todo to disabled
     setTodos([...todos]);
   };
 
-
-  const deleteTodo = (v, i) => {
-    const oldTodos = [...todos];
-    oldTodos.splice(i, 1);
-    console.log(i,"<<< i")
-    setTodos([...oldTodos]);
+  const deleteTodo = (i) => {
+    const oldTodos = [...todos]; // Poorani list ko copy karlo
+    oldTodos.splice(i, 1); // Us specific index se item delete karo
+    setTodos(oldTodos); // Update todos
   };
 
   return (
     <div>
-      <input type="text" onChange={(e) => setValue(e.target.value)} />
-      <button onClick={() => addTodos()}>Add Todos</button>
+      <input 
+        type="text" 
+        value={value} // Value ko input ke sath bind karo
+        onChange={(e) => setValue(e.target.value)} 
+      />
+      <button onClick={addTodos}>Add Todos</button>
       <button onClick={() => setTodos([])}>Delete all</button>
       <br />
 
@@ -49,16 +123,20 @@ const PracticeTodoapp = () => {
           <li key={i}>
             <input
               type="text"
-              defaultValue={v.value}
+              value={v.value} // Default value ko value mein badlo
               disabled={v.disabled}
-              onChange={(e) => (v.value = e.target.value)}
+              onChange={(e) => {
+                const newTodos = [...todos];
+                newTodos[i].value = e.target.value;
+                setTodos(newTodos);
+              }}
             />
             {v.disabled ? (
               <button onClick={() => editTodo(v, i)}>Edit</button>
             ) : (
               <button onClick={() => updateTodo(v, i)}>Update</button>
             )}
-            <button onClick={() => deleteTodo(v, i)}>Delete</button>
+            <button onClick={() => deleteTodo(i)}>Delete</button> {/* Index pass karo yahan */}
           </li>
         ))}
       </ul>
